3d7846ac6f13577a75aab50d35f8caf9c017f03e
fdai7920 <pascal.grosch@informatik.hs-fulda.de>
2025-01-27 19:15:29 +0100

    helloWorld

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,73 +1,50 @@
 package org.example;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-class IntComparable implements Comparable {
+class IntComparable implements Comparable<IntComparable> {
     private final int value;
 
     public IntComparable(int value) {
         this.value = value;
     }
 
-    public int getValue() {
-        return value;
+    @Override
+    public int compareTo(IntComparable x) {
+        return this.value - x.value;
     }
 
     @Override
-    public int compareTo(Object x) {
-        return this.value - ((IntComparable) x).value;
+    public String toString() {
+        return String.valueOf(value);
     }
 }
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        RBTree tree = new RBTree();
+        RBTree<IntComparable> tree = new RBTree<>();
         Random rand = new Random();
 
-        // bei Belieben ändern:
-        String pfad = "C:\\Users\\Admin\\Desktop\\DOT\\Tree"; //-----------------------------------------------------------------------------------------------
-
+        String pfad = "C:\\Users\\Admin\\Desktop\\DOT\\Tree";
         ArrayList<Integer> arrayList = new ArrayList<>();
-        int currentDot = 10; // startet bei 10 für die Reihenfolge von PDFs
+        int currentDot = 10;
 
-        System.out.println("Einfügen von 15 zufälligen Zahlen in den RB-Baum:");
         for (int i = 0; i < 15; i++) {
-            int num = 0;
-            // Zufallszahlen sollen sich nicht wiederholen, aber auch nicht möglicherweise in einer while(true) Schleife
-            // gefangen sein
+            int num;
             for (int j = 0; j < 1000000; j++) {
-                num = rand.nextInt(100); // Zufallszahl zwischen 0 und 99
-                if (!arrayList.contains(num)) break;
+                num = rand.nextInt(100);
+                if (!arrayList.contains(num)) {
+                    arrayList.add(num);
+                    System.out.println("Einfügen: " + num);
+                    tree.insert(new IntComparable(num));
+                    tree.printDOT(pfad + currentDot + ".dot");
+                    currentDot++;
+                    break;
+                }
             }
-            arrayList.add(num);
-            System.out.println("Einfügen: " + num);
-            tree.insertNode(new RBTree.Node(num));
-
-            //printDot mit variablen Pfad
-            tree.printDOT(pfad + currentDot + ".dot");
-            currentDot++;
         }
-
-        // Testcase: (oben tree.insertNode bis currentDot++ auskommentieren)
-
-        //int[] test = {74, 94, 72, 13, 80, 4, 61, 3, 90, 27, 51, 79, 76, 43, 44};
-        //for (int j : test) {
-            //System.out.println(j);
-            //tree.insertNode(new RBTree.Node(j));
-            //tree.printDOT("C:\\Users\\Admin\\Desktop\\DOT\\Tree" + currentDot + ".dot");
-            currentDot++;
-        //}
     }
 }
-
-// Windows ToolChain für einen Order DOT auf dem Desktop (verwendete Tools: graphviz(dot), inkscape(inkscape), pdfk(Pfad angegeben):
-
-//     Get-ChildItem .\Desktop\DOT\*.dot | ForEach-Object { & "dot" -Tsvg $_.FullName > "$($_.FullName -replace '\.dot$', '.svg')" }
-//
-//     Get-ChildItem .\Desktop\DOT\*.svg | ForEach-Object { & "inkscape" $_.FullName --export-filename "$($_.FullName -replace '\.svg$', '.pdf')" }
-//
-//     C:\"Program Files (x86)"\PDFtk\bin\pdftk.exe .\Desktop\DOT\*.pdf cat output .\Desktop\combined.pdf
--- src/main/java/org/example/RBTree.java
+++ src/main/java/org/example/RBTree.java
@@ -1,234 +1,221 @@
 package org.example;
+
 import java.io.*;
 
-interface Comparable {
-    public int compareTo(Object x);
+interface Comparable<T> {
+    int compareTo(T x);
 }
 
-public class RBTree {
+public class RBTree<T extends Comparable<T>> {
     private enum Color {
         RED, BLACK
     }
 
-    public static class Node implements Comparable {
-        int value;
+    public static class Node<T extends Comparable<T>> {
+        T value;
         Color color;
-        Node left, right, parent;
+        Node<T> left, right, parent;
 
-        Node(int value) {
+        Node(T value) {
             this.value = value;
             this.color = Color.RED;
         }
-
-        @Override
-        public int compareTo(Object x) {
-            return this.value - ((Node)x).value;
-        }
     }
 
-    private Node root;
+    private Node<T> root;
 
     public RBTree() {
         root = null;
     }
 
-    public void insertNode(Node newNode) {
-        Node node = root; Node parent = null;
-        // Traverse the tree to the left or right depending on the key
+    public void insert(T value) {
+        Node<T> newNode = new Node<>(value);
+        insertNode(newNode);
+    }
+
+    private void insertNode(Node<T> newNode) {
+        Node<T> node = root, parent = null;
         while (node != null) {
             parent = node;
-            if (newNode.compareTo(node) < 0) {
+            if (newNode.value.compareTo(node.value) < 0) {
                 node = node.left;
-            } else if (newNode.compareTo(node) > 0) {
+            } else if (newNode.value.compareTo(node.value) > 0) {
                 node = node.right;
             } else {
-                throw new IllegalArgumentException("BST already contains a node with key " + newNode.value);
+                throw new IllegalArgumentException("BST already contains value: " + newNode.value);
             }
         }
-        // Insert new node
+
         newNode.color = Color.RED;
+        newNode.parent = parent;
         if (parent == null) {
             root = newNode;
-        } else if (newNode.compareTo(parent) < 0) {
+        } else if (newNode.value.compareTo(parent.value) < 0) {
             parent.left = newNode;
         } else {
             parent.right = newNode;
         }
-        newNode.parent = parent;
+
         fixRedBlackPropertiesAfterInsert(newNode);
     }
 
-    public void fixRedBlackPropertiesAfterInsert(Node newNode) {
-        // Fall 1: Der neue Knoten ist die Wurzel
-        // Fall 2: Der Vater ist die Wurzel und rot
-        if (root.color == Color.RED) {
-            root.color = Color.BLACK;
-            return;
-        }
+    private void fixRedBlackPropertiesAfterInsert(Node<T> node) {
+        while (node != root && node.parent.color == Color.RED) {
+            Node<T> parent = node.parent;
+            Node<T> grandParent = parent.parent;
+            if (grandParent == null) break;
 
-        Node grandParent = newNode.parent.parent;
-        if (grandParent == null) {
-            return;
-        }
+            boolean isLeftChild = (parent == grandParent.left);
+            Node<T> uncle = isLeftChild ? grandParent.right : grandParent.left;
 
-        // Fall 3: Vater und Onkelknoten sind rot
-        // Bestimmen, ob der Vater links oder rechts vom Großvater ist
-        if (newNode.parent == grandParent.left) {
-            Node uncle = grandParent.right;
-
-            // Fall A: Onkel ist rot => Recolor und höher prüfen
             if (uncle != null && uncle.color == Color.RED) {
-                newNode.parent.color = Color.BLACK;
+                parent.color = Color.BLACK;
                 uncle.color = Color.BLACK;
                 grandParent.color = Color.RED;
-                fixRedBlackPropertiesAfterInsert(grandParent);
-            }
-            // Fall B: Onkel ist schwarz
-            else {
-                // innerer Enkel => Linksdrehung beim Vater
-                if (newNode == newNode.parent.right) {
-                    rotateLeft(newNode.parent);
-                    // Nach der Rotation ist newNode.parent gewechselt
-                    // Verschiebe Fokus auf das ehemals "linke Kind"
-                    newNode = newNode.left;
+                node = grandParent;
+            } else {
+                if (isLeftChild && node == parent.right) {
+                    rotateLeft(parent);
+                    node = parent;
+                    parent = node.parent;
+                } else if (!isLeftChild && node == parent.left) {
+                    rotateRight(parent);
+                    node = parent;
+                    parent = node.parent;
                 }
-                // äußerer Enkel => Rechtsdrehung beim Großvater
-                newNode.parent.color = Color.BLACK;
+                parent.color = Color.BLACK;
                 grandParent.color = Color.RED;
-                rotateRight(grandParent);
-            }
-        } else {
-            // Vater ist rechtes Kind vom grandParent
-            Node uncle = grandParent.left;
-
-            // Fall A: Onkel ist rot
-            if (uncle != null && uncle.color == Color.RED) {
-                newNode.parent.color = Color.BLACK;
-                uncle.color = Color.BLACK;
-                grandParent.color = Color.RED;
-                fixRedBlackPropertiesAfterInsert(grandParent);
-            }
-            // Fall B: Onkel ist schwarz
-            else {
-                // innerer Enkel => Rechtsdrehung beim Vater
-                if (newNode == newNode.parent.left) {
-                    rotateRight(newNode.parent);
-                    newNode = newNode.right;
+                if (isLeftChild) {
+                    rotateRight(grandParent);
+                } else {
+                    rotateLeft(grandParent);
                 }
-                // äußerer Enkel => Linksdrehung beim Großvater
-                newNode.parent.color = Color.BLACK;
-                grandParent.color = Color.RED;
-                rotateLeft(grandParent);
             }
         }
-
-        // Fall 4: Vater ist rot, Onkel ist schwarz, Knoten ist innerer Enkel
-        // Fall 5: Vater ist rt, Onkel ist schwarz, Knoten ist äußerer Enkel
-        if (newNode.parent.color == Color.RED && newNode == newNode.parent.left) {
-            rotateRight(newNode.parent);
-            rotateLeft(newNode.parent.parent);
-
-            newNode.parent.color = Color.BLACK;
-            newNode.parent.parent.color = Color.RED;
-        } else if (newNode.parent.color == Color.RED && newNode == newNode.parent.right) {
-            rotateLeft(newNode.parent.parent);
-
-            newNode.parent.color = Color.BLACK;
-            newNode.parent.parent.color = Color.RED;
-        }
+        root.color = Color.BLACK;
     }
 
-    private void rotateRight(Node node) {
-        Node parent = node.parent;
-        Node leftChild = node.left;
-
-        node.left = leftChild.right;
-        if (leftChild.right != null) {
-            leftChild.right.parent = node;
-        }
-
-        leftChild.right = node;
-        node.parent = leftChild;
-        replaceParentsChild(parent, node, leftChild);
-    }
-
-    private void rotateLeft(Node node) {
-        Node parent = node.parent;
-        Node rightChild = node.right;
+    private void rotateLeft(Node<T> node) {
+        Node<T> rightChild = node.right;
         node.right = rightChild.left;
         if (rightChild.left != null) {
             rightChild.left.parent = node;
         }
+        rightChild.parent = node.parent;
+        replaceParentChild(node.parent, node, rightChild);
         rightChild.left = node;
         node.parent = rightChild;
-        replaceParentsChild(parent, node, rightChild);
     }
 
-    private void replaceParentsChild(Node parent, Node oldChild, Node newChild) {
+    private void rotateRight(Node<T> node) {
+        Node<T> leftChild = node.left;
+        node.left = leftChild.right;
+        if (leftChild.right != null) {
+            leftChild.right.parent = node;
+        }
+        leftChild.parent = node.parent;
+        replaceParentChild(node.parent, node, leftChild);
+        leftChild.right = node;
+        node.parent = leftChild;
+    }
+
+    private void replaceParentChild(Node<T> parent, Node<T> oldChild, Node<T> newChild) {
         if (parent == null) {
             root = newChild;
         } else if (parent.left == oldChild) {
             parent.left = newChild;
-        } else if (parent.right == oldChild) {
-            parent.right = newChild;
         } else {
-            throw new IllegalStateException("Node is not a child of its parent");
+            parent.right = newChild;
         }
         if (newChild != null) {
             newChild.parent = parent;
         }
     }
 
-    private static Object[][] nodeParents = new Object[10000][3];
-    private void inorder(Node n) {
+    private static Object[][] nodeParents = new Object[10000][4];
+    private void inorder(Node<T> n) {
         int i = 0;
-        if (n.left != null) inorder(n.left);
-        if (n.parent != null) {
+        if (n.left != null) inorder(n.left); // solange links suchen bis keine Knoten mehr da sind
 
-            while (nodeParents[i][0] != null) {
-                i++;
-            }
-            nodeParents[i][0] = n.value;
-            nodeParents[i][1] = n.color;
-            nodeParents[i][2] = n.parent.value;
 
-        }
-        if (n.right != null) inorder(n.right);
+        while (nodeParents[i][0] != null) {
+            i++;
+        }         // an die nächste neue Stelle gehen
+
+        nodeParents[i][0] = n.value; // Value speichern
+        nodeParents[i][1] = n.color; // Color speichern
+        if (n.parent != null) nodeParents[i][2] = n.parent.value; // ElternValue speichern, wenn es nicht root ist
+        else nodeParents[i][2] = -1; // sonst -1
+        if (n.left != null && n.right == null) nodeParents[i][3] = 0;  // ein Elternteil ohne rechtes Kind (für Invis)
+        if (n.left == null && n.right != null) nodeParents[i][3] = 1; // ein Elternteil ohne linkes Kind (für Invis)
+
+        if (n.right != null) inorder(n.right); // solange rechts suchen bis keine Knoten mehr da sind
 
     }
     public void printDOT(String file) throws IOException {
-        nodeParents = new Object[10000][3];
+        nodeParents = new Object[10000][4]; // Array erstmal (wieder) leeren
+
+        // Aufbau der Datei
         String startText = "digraph G {\n" +
                 "\tgraph [ratio=.48];\n" +
                 "\tnode [style=filled, color=black, shape=circle, width=.6 \n" +
                 "\t\tfontname=Helvetica, fontweight=bold, fontcolor=white, \n" +
                 "\t\tfontsize=24, fixedsize=true];\n" +
                 "\t\n\n";
-        String order = "  ";
-        String redDots = "  ";
+        String leftSingleParents = "  "; // linke Werte müssen als Erstes initialisiert werden in dem Programm (hier die Geisternodes)
+        String order = "  "; // linke Werte müssen als Erstes initialisiert werden
+        String rightSingleParents = "  ";
+        String redDots = "  "; // alle Knoten, die rot sind, so färben
         String redText =
                 "\t\n" +
                 "\t[fillcolor=red];\n" +
                 "\n";
-        String connectDots = "";
+        String connectDots = ""; // die Verbindungen anzeigen
         String endText = "\n" + "}";
 
 
-        // connect inOrder
+        // über inOrder nodeParents befüllen
         inorder(root);
         int i = 0;
 
+        // jede Stelle durchgehen
         while (nodeParents[i][0] != null) {
-            if (nodeParents[i][1] == Color.RED) redDots = redDots.concat(nodeParents[i][0].toString() + ", ");
-            connectDots = connectDots.concat(nodeParents[i][2] + " -> " + nodeParents[i][0] + "; \n");
-            order = order.concat(nodeParents[i][0] + ", ");
+            int parentID = 0;
+            // falls nicht root, finde das Elternteil
+            if (nodeParents[i][0] != (Object) root.value)
+            {
+                while (nodeParents[parentID][0] != null) {
+                    if (nodeParents[i][2] == nodeParents[parentID][0]) break;
+                    parentID++;
+                }
+            }
+
+            // i --> Index vom Array, i+1 --> Index für Graphviz
+            if (nodeParents[i][1] == Color.RED) redDots = redDots.concat((i+1) + ", "); // falls rot, zu redDots hinzufügen
+            if (nodeParents[i][2] != (Object) (-1)) connectDots = connectDots.concat(parentID+1 + " -> " + (i+1) + "; \n"); // falls nicht die root, mit Elternteil verknüpfen
+            order = order.concat((i+1) + " [label=\"" + nodeParents[i][0] + "\"]; \n "); // zur Reihenfolge hinzufügen
+
+            // falls links kein Knoten ist, Geistknoten hinzufügen für die Struktur
+            if (nodeParents[i][3] == (Object) 1)  {
+                leftSingleParents = leftSingleParents.concat((1000+i) + " [label=\"\",style=invis]; \n");
+                connectDots = connectDots.concat(i+1 + " -> " + (1000+i) + " [style=invis]; \n");
+            }
+            // falls rechts kein Knoten ist, Geistknoten hinzufügen für die Struktur
+            else if (nodeParents[i][3] == (Object) 0) {
+                rightSingleParents = rightSingleParents.concat((1000+i) + " [label=\"\",style=invis]; \n");
+                connectDots = connectDots.concat(i+1 + " -> " + (1000+i) + " [style=invis]; \n");
+            }
             i++;
         }
 
+        // hier haben wir mit Kommas gearbeitet, die dann vom letzten Element jeweils wieder entfernt werden müssen
         redDots = redDots.substring(0, redDots.length()-2);
-        order = order.substring(0, order.length() -2) + ";\n\n";
-        String input = startText.concat(order + redDots + redText + connectDots + endText);
-        if (redDots.isEmpty()) input = startText.concat(root.value + ";" + endText);
+        order = order.substring(0, order.length() -2) + "\n\n";
+
+        // die Reihenfolge der String-Elemente der Datei
+        String input = startText.concat(leftSingleParents + order + rightSingleParents + redDots + redText + connectDots + endText);
+        // falls nur die Root existiert
+        if (redDots.isEmpty()) input = startText.concat("1 [label=\"" + root.value + "\"];" + endText);
 
         File out = new File(file);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(out))) {
@@ -236,9 +223,5 @@ public class RBTree {
             writer.write(input);
             writer.flush();
         }
-
-
-
-
     }
 }
